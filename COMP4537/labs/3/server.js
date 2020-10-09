38e3276a71401dd79e6f1443287d64f2ffba6ef2
debby <debbychiu@outlook.com>
2020-10-09 16:57:43 -0700

    fixes to link pathing

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,6 +1,6 @@
 function submit() {
 	let name = document.getElementById('name').value;
-	fetch(`https://morning-thicket-36286.herokuapp.com/?name=${name}`)
+	fetch(`https://amir-thing.herokuapp.com/?name=${name}`)
 	.then(response => response.json())
 	.then(data => document.getElementById('message').innerHTML = data['res']);
 }
