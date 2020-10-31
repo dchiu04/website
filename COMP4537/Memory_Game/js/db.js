9274d4caddc57448dc9f5d20f3a7b5b1a47b46f7
debby <debbychiu@outlook.com>
2020-10-31 05:03:25 -0700

    finished leaderboard

--- COMP4537/Memory_Game/js/db.js
+++ COMP4537/Memory_Game/js/db.js
@@ -5,8 +5,7 @@ function genLeaderBoard() {
 	
 	let name = localStorage.getItem("name");
 
-	
-	//won't let 0 score
+	//crashes if score is undefined
 	if(score === undefined || !name ) {
 		location.href="./index.html"
 		return
@@ -19,109 +18,43 @@ function genLeaderBoard() {
 	// Default options are marked with *
 	fetch(url, {
 		method: 'POST', // *GET, POST, PUT, DELETE, etc.
-		// mode: 'no-cors', // no-cors, *cors, same-origin
-		// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-		// credentials: 'omit', // include, *same-origin, omit
-		// headers: {
-		// 	'Content-Type': 'application/json'
-		// 	// 'Content-Type': 'application/x-www-form-urlencoded',
-		// },
-		// redirect: 'follow', // manual, *follow, error
-		// referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 		body: JSON.stringify(data) // body data type must match "Content-Type" header
-		}).then(response => response.text()).then(data => {
-			document.getElementById("user").innerHTML = name + " " + score;
+		})
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById("user").innerHTML = name + "<br>" + "Score: " + score;
 			fetch("https://wicked-spider-23736.herokuapp.com/get_leaderboard")
-			.then(response => response.json()).then(data => {console.log(data)}).catch(e => console.log(e))
-		}).catch(e => console.log(e))
+			.then(response => response.json())
+			.then(data => { //data is array of leaderboard
+				//console.log(data);
+
+				const objectToValuesPolyfill = (data) => {
+					return Object.keys(data).map(key => data[key]);
+				};
+
+				  Object.values = Object.values || objectToValuesPolyfill;
+
+				var array = Object.values(data);
+				console.log(array[0][1].name);
+
+				let board = document.getElementById("board");
+
+				//header
+				let b = "<tr><th>Rank</th><th>Name</th><th>Score</th>";
+				
+				//top 5 players
+				for(let i = 0; i < 5; i++) {
+					b += "<tr>"
+					b += "<td>" + (i+1) + "</td>"
+					b += "<td>" + array[0][i].name + "</td>" //name
+					b += "<td>" + array[0][i].score + "</td></tr>" //score
+					b += "</tr>"
+				}
+				board.innerHTML = b;
+			})
+			
+		})
 	};
 
 
 
-function getData() {
-	//req.body.name //name
-	//req.body.score //score
-}
-
-/**
-function fetch(name, score) {
-	let req = fetch("link"),
-	{
-		method: 'POST',
-		headers:
-		{
-			"Accept": "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(
-		{
-			"username": name,
-			"score": score.toString()
-		})
-	}
-	.then(response => response.json())
-	.then(data ->
-	{
-		console.log("Success:", data)
-	})
-	.catch((error) =>
-	{
-		console.error("Error:", error)
-	});
-	return answer;
-}
-
-//app.fetch("link")
-
-/**
-'use strict';
-//const express = require('express');
-var mysql = require('mysql');
-
-//const app = express();
-
-var con = mysql.createConnection({
-    host: "sql3.freemysqlhosting.net",
-    user: "sql3366017",
-    password: "T5kPxgQmTA",
-    database: "sql3366017",
-	
-});
-
-con.connect(function(err) {
-	console.log("trying to connect");
-  if (err) throw err;
-  console.log("Connected!");
-});
-*/
-/**
-processQueryResults = (results) => {
-    return JSON.parse(JSON.stringify((results[0])));
-}
-
-
-app.get('/get_leaderboard', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    con.query(`CALL GetScores();`, (error, results, fields) => {
-        if (error) res.send({'success': false, 'error': error});
-        else res.send({'success': true, 'payload': processQueryResults(results)});
-    });
-});
-
-
-app.get('/submit_a_score', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    let score = req.query.score;
-    let name = req.query.name;
-    con.query(`CALL AddScoreToLeaderboard('${name}', ${score})`, (error, results, fields) => {
-        if (error) res.send({'success': false, 'error': error});
-        else res.send({'success': true, 'payload': processQueryResults(results)[0]});
-    });
-});
-
-var port = 8000 || process.env.PORT
-
-app.listen(port);
-
-console.log(`listening on port ${port}`);
-*/
